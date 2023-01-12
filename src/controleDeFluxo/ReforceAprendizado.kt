8d460b44912c03d828dee069e1ef46bb87bac269
Fabricio Calvanese <fabriciocalvanese@gmail.com>
2023-01-12 19:11:10 -0300

    Add When and ReforceAprendizado files

---
+++ src/controleDeFluxo/ReforceAprendizado.kt
@@ -0,0 +1,9 @@
+package controleDeFluxo
+
+fun main() {
+
+}
+
+/*Ex 1 -> Escreva um programa capaz de ler dois números inteiros que representam os lados de uma figura
+geométrica. Informar se formam um quadrado (lados iguais).
+*/
---
+++ src/controleDeFluxo/When.kt
@@ -0,0 +1,46 @@
+package controleDeFluxo
+
+fun main() {
+    val n = readLine()
+    println(n)
+}
+
+fun bonus(cargo: String): Float {
+    var bonus = 0f
+
+    if (cargo == "Gerente") {
+        bonus = 2000f
+    } else if (cargo == "Coordenador") {
+        bonus = 1500f
+    } else if (cargo == "Engenheiro de software") {
+        bonus = 1000f
+    } else if (cargo == "Estagiários") {
+        bonus = 500f
+    }
+
+    return bonus
+}
+
+fun bonusWhen(cargo: String): Float {
+    var bonus = 0f
+
+    when (cargo) {
+        "Gerente" -> bonus = 2000f
+        "Coordenador" -> bonus = 1500f
+        "Engenheiro de software" -> bonus = 1000f
+        "Estagiário" -> bonus = 500f
+    }
+    return bonus
+}
+
+//Outra forma:
+
+fun bonusWhen2(cargo: String): Float {
+    return when (cargo) {
+        "Gerente" -> 2000f
+        "Coordenador" -> 1500f
+        "Engenheiro de software" -> 1000f
+        "Estagiário" -> 500f
+        else -> 0f
+    }
+}
