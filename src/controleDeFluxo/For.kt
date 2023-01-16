6e360880fd69a1f6612ab3e071c77f79a617b41e
Fabricio Calvanese <fabriciocalvanese@gmail.com>
2023-01-16 18:11:26 -0300

    Update For file

--- src/controleDeFluxo/For.kt
+++ src/controleDeFluxo/For.kt
@@ -2,24 +2,24 @@ package controleDeFluxo
 
 fun main() {
 
-    for(i in 1..10){
+    for (i in 1..10) {
         println("$i ")
     }
 
-    for(i in 0..20 step 2){
+    for (i in 0..20 step 2) {
         println("$i ")
     }
 
-    for(i in 30 downTo 0){
+    for (i in 30 downTo 0) {
         println("$i ")
     }
 
-    for(i in 30 downTo 0 step 3){
+    for (i in 30 downTo 0 step 3) {
         println("$i ")
     }
 
     val str = "Kotlin is great!!"
-    for(char in str){
+    for (char in str) {
         println("$char ")
     }
 }
