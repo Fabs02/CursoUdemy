b44a367368db21252db8cf88b178755fccd9afd3
Fabricio Calvanese <fabriciocalvanese@gmail.com>
2023-01-16 18:03:23 -0300

    Add For file

---
+++ src/controleDeFluxo/For.kt
@@ -0,0 +1,26 @@
+package controleDeFluxo
+
+fun main() {
+
+    for(i in 1..10){
+        println("$i ")
+    }
+
+    for(i in 0..20 step 2){
+        println("$i ")
+    }
+
+    for(i in 30 downTo 0){
+        println("$i ")
+    }
+
+    for(i in 30 downTo 0 step 3){
+        println("$i ")
+    }
+
+    val str = "Kotlin is great!!"
+    for(char in str){
+        println("$char ")
+    }
+}
+
