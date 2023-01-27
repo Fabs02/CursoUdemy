91fbc1291844241d533247ff781ed3be2856a01a
Fabricio Calvanese <fabriciocalvanese@gmail.com>
2023-01-27 17:02:35 -0300

    git commit -m "Add While and DoWhile files"

---
+++ src/controleDeFluxo/DoWhile.kt
@@ -0,0 +1,10 @@
+package controleDeFluxo
+
+fun main() {
+
+    do{
+        println("Qual o seu nome?: ")
+        val nome = readLine()
+    }while (nome == "")
+
+}
---
+++ src/controleDeFluxo/While.kt
@@ -0,0 +1,25 @@
+package controleDeFluxo
+
+fun main() {
+
+    var i = 0
+
+    while (i <= 100) {
+        println("$i")
+        i++
+
+    }
+
+    kotlin()
+}
+
+fun kotlin() {
+
+    val k = "Kotlin"
+    var i = 0
+
+    while (i < k.length) {
+        println("${k[i]}")
+        i++
+    }
+}
