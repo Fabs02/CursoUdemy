1d00c74dd6aa9b593f4e70253f2a85c7833b1c84
Fabricio Calvanese <fabriciocalvanese@gmail.com>
2024-01-29 16:08:02 -0300

    Reformat Exercício file from conceitosIniciais

--- src/conceitosIniciais/Exercício.kt
+++ src/conceitosIniciais/Exercício.kt
@@ -2,11 +2,11 @@ fun main() {
 
     var nome = "Fabricio Calvanese Augusto"
     var textoVazio = ""
-    val calcado:Byte = 44
+    val calcado: Byte = 44
     val PIB: Long = 1869000000000
     val populacao: Int = 211000000
 
-    val PIBPercapita = PIB/populacao
+    val PIBPercapita = PIB / populacao
 
     println("R$ $PIBPercapita")
 }
